ddebaafe44c3998f135137156c7b483072dc8245
yameriong <sexymapxy@gmail.com>
2018-06-04 18:17:29 +0900

    Initial commit

---
+++ src/main/java/ac/bsedu/kindergarden/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package ac.bsedu.kindergarden.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "HelloWorld";
+  }
+}
